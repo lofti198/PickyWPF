a8e3efff6476f96a740986e36c196de06a7e588f
lofti198 <isolar2005@gmail.com>
2024-03-05 07:06:43 +0200

    browser somehow added +  models for mvvm started

---
+++ PickyWPF/Models/ActionCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace PickyWPF.Models
+{
+    public class ActionCommand : ICommand
+    {
+        private readonly Action<object> _execute;
+
+        public ActionCommand(Action<object> execute)
+        {
+            _execute = execute;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            // This can be used to define custom logic for enabling/disabling the command
+            // For example, return true if certain conditions are met.
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+    }
+}
---
+++ PickyWPF/Models/Tab.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+using System.Windows.Input;
+
+namespace PickyWPF.Models
+{
+  
+
+    public interface ITab
+
+    {
+
+        string Name { get; set; }
+
+        ICommand CloseCommand { get; }
+
+        event EventHandler CloseRequested;
+
+    }
+
+    public abstract class Tab : ITab
+
+    {
+
+        public Tab()
+        {
+            CloseCommand = new ActionCommand(p => CloseRequested?.Invoke(this, EventArgs.Empty));
+        }
+
+        public string Name { get; set; }
+
+        public ICommand CloseCommand { get; }
+
+        public event EventHandler CloseRequested;
+
+    }
+
+
+}
