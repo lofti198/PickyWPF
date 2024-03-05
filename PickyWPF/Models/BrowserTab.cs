7a2511bb766c49803eb2849260995229aa9d831a
lofti198 <isolar2005@gmail.com>
2024-03-05 08:35:08 +0200

    basic layout and MVVM structure

---
+++ PickyWPF/Models/BrowserTab.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PickyWPF.Models
+{
+    public class BrowserTab : Tab
+    {
+        public BrowserTab()
+        {
+            Name = "Page title";
+        }
+    }
+}
---
+++ PickyWPF/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,32 @@
+﻿using PickyWPF.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace PickyWPF.ViewModels
+{
+    public class MainWindowViewModel
+    {
+
+        public MainWindowViewModel()
+        {
+            NewTabCommand = new ActionCommand(p => NewTab());
+            Tabs = new ObservableCollection<ITab>();
+            Tabs.Add(new BrowserTab());
+        }
+
+        public ICommand NewTabCommand { get; }
+        public ICollection<ITab> Tabs { get; }
+
+        private void NewTab()
+        {
+            // Add logic here to create a new ITab and add it to the Tabs collection
+            // For example:
+            // Tabs.Add(new MyTabItem { Name = $"Tab {Tabs.Count + 1}" });
+        }
+    }
+}
